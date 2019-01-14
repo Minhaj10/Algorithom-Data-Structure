c9834745b0bc750c0ea9d5e489bf5a510833e6be
minhajumid <minhajumid9897@gmail.com>
2019-01-14 23:12:11 +0600

    Insertion sort

---
+++ Algorithom/insertion sort.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int ar[100],a,b,c,d,n;
+    cout<<"Number of sheep-->";
+    cin>>n;
+    cout<<"The number you want to sort from backward-->";
+    cin>>a;
+    for(int i=0;i<n;i++){
+
+        cin>>ar[i];
+    }
+    for(int i=1;i<n;i++){
+        d=ar[i];
+        int j=i-1;
+        while(j>=n-a && ar[j]>=d){
+            ar[j+1]=ar[j];
+            j--;
+        }
+        ar[j+1]=d;
+    }
+    for(int i=0;i<n;i++){
+        cout<<ar[i]<<" ";
+    }
+    return 0;
+}
