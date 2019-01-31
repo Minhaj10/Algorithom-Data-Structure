53f3a7de5fa8abb9d44ef1e452af8a729d5ed773
minhajumid <minhajumid9897@gmail.com>
2019-01-31 21:03:10 +0600

    Update of activity selection

--- Algorithom/Activity selection.cpp
+++ Algorithom/Activity selection.cpp
@@ -1,59 +1,52 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <set>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Pair
+struct Activitiy
 {
-
-	int start, finish;
+    int start, finish;
 };
 
 
-void selectActivity(vector<Pair> const &vec)
+bool activityCompare(Activitiy s1, Activitiy s2)
 {
-
-	int k = 0;
-
-
-	set<int> out;
-
-
-	out.insert(0);
-
-	for (int i = 1; i < vec.size(); i++)
-	{
-
-		if (vec[i].start >= vec[k].finish)
-		{
-			out.insert(i);
-			k = i;
-		}
-	}
-
-	for (int i: out) {
-		cout << "{" << vec[i].start << ", " << vec[i].finish << "}" << '\n';
-	}
+    return (s1.finish < s2.finish);
 }
 
 
-int main()
+void printMaxActivities(Activitiy arr[], int n)
 {
 
-	vector<Pair> activities =
-	{
-		{1, 4}, {3, 5}, {0, 6}, {5, 7}, {3, 8}, {5, 9},
-		{6, 10}, {8, 11}, {8, 12}, {2, 13}, {12, 14}
-	};
+    sort(arr, arr+n, activityCompare);
+
+    cout << "Following activities are selected n";
 
 
-	sort(activities.begin(), activities.end(),
-		[](auto const &lhs, auto const &rhs) {
-			return lhs.finish < rhs.finish;
-		});
+    int i = 0;
+    int flag=0;
 
-	selectActivity(activities);
 
-	return 0;
+
+    for (int j = 1; j < n; j++)
+    {
+      if (arr[j].start >= arr[i].finish)
+      {
+          flag++;
+          i = j;
+      }
+    }
+    cout<<"Total activity "<<flag+1<<endl;
+}
+
+int main()
+{
+   Activitiy ar[10000];
+   int n;
+   cin>>n;
+   for(int i=0;i<n;i++){
+    cin>>ar[i].start;
+    cin>>ar[i].finish;
+   }
+
+    printMaxActivities(ar, n);
+    return 0;
 }
