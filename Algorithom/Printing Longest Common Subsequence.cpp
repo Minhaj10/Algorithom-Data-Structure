32582788238559296b6b4e30d1bbc981a9bc0229
minhajumid <minhajumid9897@gmail.com>
2019-02-04 00:25:29 +0600

    Update

--- Algorithom/Printing Longest Common Subsequence.cpp
+++ Algorithom/Printing Longest Common Subsequence.cpp
@@ -1,69 +1,60 @@
-
-#include<iostream>
-#include<cstring>
-#include<cstdlib>
+#include <bits/stdc++.h>
 using namespace std;
 
-/* Returns length of LCS for X[0..m-1], Y[0..n-1] */
-void lcs( string X, string Y, int m, int n )
+
+void printLCS( char *s1, char *s2, int m, int n )
 {
-   int L[m+1][n+1];
+    int L[m+1][n+1];
 
-   /* Following steps build L[m+1][n+1] in bottom up fashion. Note
-      that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
-   for (int i=0; i<=m; i++)
-   {
-     for (int j=0; j<=n; j++)
-     {
-       if (i == 0 || j == 0)
-         L[i][j] = 0;
-       else if (X[i-1] == Y[j-1])
-         L[i][j] = L[i-1][j-1] + 1;
-       else
-         L[i][j] = max(L[i-1][j], L[i][j-1]);
-     }
-   }
 
-   // Following code is used to print LCS
-   int index = L[m][n];
+    for (int i=0; i<=m; i++)
+    {
+        for (int j=0; j<=n; j++)
+        {
+            if (i == 0 || j == 0)
+                L[i][j] = 0;
+            else if (s1[i-1] == s2[j-1])
+                L[i][j] = L[i-1][j-1] + 1;
+            else
+                L[i][j] = max(L[i-1][j], L[i][j-1]);
+        }
+    }
 
-   // Create a character array to store the lcs string
-   char lcs[index+1];
-   lcs[index] = 'u'; // Set the terminating character
 
-   // Start from the right-most-bottom-most corner and
-   // one by one store characters in lcs[]
-   int i = m, j = n;
-   while (i > 0 && j > 0)
-   {
-      // If current character in X[] and Y are same, then
-      // current character is part of LCS
-      if (X[i-1] == Y[j-1])
-      {
-          lcs[index-1] = X[i-1]; // Put current character in result
-          i--; j--; index--;     // reduce values of i, j and index
-      }
+    int index = L[m][n];
 
-      // If not same, then find the larger of two and
-      // go in the direction of larger value
-      else if (L[i-1][j] > L[i][j-1])
-         i--;
-      else
-         j--;
-   }
+    char LCS[index+1];
+    LCS[index] = '\0';
 
-   // Print the lcs
-   cout << "LCS of " << X << " and " << Y << " is " << lcs;
+
+    int i = m, j = n;
+    while (i > 0 && j > 0)
+    {
+
+        if (s1[i-1] == s2[j-1])
+        {
+            LCS[index-1] = s1[i-1];
+            i--;
+            j--;
+            index--;
+        }
+
+        else if (L[i-1][j] > L[i][j-1])
+            i--;
+        else
+            j--;
+    }
+
+
+    cout << "LCS of " << s1 << " and " << s2 << " is "<<endl << LCS<<endl;
 }
 
-/* Driver program to test above function */
+
 int main()
 {
-  string X,Y;
-  cin>>X>>Y;
-  int m = X.length();
-  int n = Y.length();
-
-  lcs(X, Y, m, n);
-  return 0;
+    char s1[10000];
+    char s2[10000] ;
+    cin>>s1>>s2;
+    printLCS(s1, s2, strlen(s1), strlen(s2));
+    return 0;
 }
