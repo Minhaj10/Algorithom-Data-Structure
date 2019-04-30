85e74e5c004a3227a91b12a57f98a66c69df51b6
unknown <minhajumid987@gmail.com>
2019-04-30 23:07:32 +0600

    merge and quick sort

---
+++ Algorithom/quick sort.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high- 1; j++)
+    {
+
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quicksort(int a[], int p, int r)
+{
+    if(p < r)
+    {
+        int q;
+        q = partition(a, p, r);
+        quicksort(a, p, q-1);
+        quicksort(a, q+1, r);
+    }
+}
+void printArray(int a[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[100000],n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    quicksort(arr,0,n-1);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
