378dd3990c49242539ba4b89cb137a7528f88c59
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2022-06-14 20:01:38 +0600

    update

---
+++ Algorithom/Codeforces/1.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define all(q) (q).begin(),(q).end()
+#define pi acos(-1)
+#define FastRead    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fore(i,a,b) for (i=a;i<b;i++)
+const int mod=1e6;
+
+//Author : @minhaj ahmed
+
+vector<pair<int,int>>adj;
+vector<int>vec[100005];
+map<pair<int,int>,int>mp;
+
+int visited[100005];
+
+int h, m, hh, mm;
+const int N=10000;
+const int MAX = 1e7;
+
+
+ll gcd(ll a, ll b)
+{
+    return b?gcd(b,a%b):a;
+}
+bool isVowel(char a)
+{
+    if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')
+        return true;
+    return false;
+}
+
+void dfs(int node,int edge)
+{
+
+    visited[node]=1;
+
+    for(auto child: vec[node])
+    {
+        if(visited[child]== 0)
+        {
+
+            dfs(child,edge);
+        }
+    }
+
+}
+
+
+void solve()
+{
+
+    ll n,a,m,b,c,d,k;
+    ll track=0;
+    ll mx;
+    ll point=20,sum=0;
+    ll flag=0;
+    string s;
+    string s1,s2;
+    set<int>st;
+
+
+    cin>>n;
+    ll arr[n+5];
+    ll arr1[n+5];
+
+
+    for(ll i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+
+
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin>>t;
+    while(t--)
+        solve();
+
+}
+
+
